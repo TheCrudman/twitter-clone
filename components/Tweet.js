7de99715675bcfb8c7a6badc3746a7eebb586ad0
TheCrudman <betterwebco@gmail.com>
2022-08-15 17:32:58 +1200

    Add fake user generation

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,8 +1,18 @@
+import Image from 'next/image'
 import timeago from 'lib/timeago'
 
 export default function Tweet({ tweet }) {
   return (
     <p>
+      {tweet.author.image && (
+        <Image
+          className="rounded-full"
+          src={tweet.author.image}
+          alt={tweet.author.name}
+          width="40"
+          height="40"
+        />
+      )}
       {timeago.format(new Date(tweet.createdAt))} {tweet.author.name}{' '}
       {tweet.content}
     </p>
